2c708402a029f45247443cc9f9e9fad77f465149
scottwdavidson <sdavi1010@gmail.com>
2018-01-22 09:30:15 -0800

    jquery initial

---
+++ jquery/ex1.js
@@ -0,0 +1,8 @@
+$("div").css("background","purple");
+$("div.highlight").css("width","200px");
+$("#third").css("border","1px solid orange");
+
+// Three options here:
+// $("div:first-of-type").first().css("color","pink");
+// $("div:first").first().css("color","pink");
+$("div").first().css("color","pink");
